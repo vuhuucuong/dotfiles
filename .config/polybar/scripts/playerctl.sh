c534c8091f38aa7595b8c0259fe81982cebaa1b8
Cuong Vu <vuhuucuong1310@gmail.com>
2020-08-08 17:48:59 +0700

    update config for bspwm

--- .config/polybar/scripts/playerctl.sh
+++ .config/polybar/scripts/playerctl.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl -p spotify status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "> $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+    echo "> - $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo "<> $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+    echo "/ - $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
 else
     echo ""
 fi
