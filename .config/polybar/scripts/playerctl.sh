e24394d6e375242d1609d6d85416fba1cfe4c437
Cuong Vu <vuhuucuong1310@gmail.com>
2020-09-18 02:47:01 +0700

    update polybar spotify

--- .config/polybar/scripts/playerctl.sh
+++ .config/polybar/scripts/playerctl.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl -p spotify status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "> - $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+    echo "[>] $(playerctl -p spotify metadata --format '{{ artist }} - {{ title }}')"
 elif [ "$player_status" = "Paused" ]; then
-    echo "/ - $(playerctl -p spotify metadata artist) - $(playerctl -p spotify metadata title)"
+    echo "[-] $(playerctl -p spotify metadata --format '{{ artist }} - {{ title }}')"
 else
-    echo ""
+    echo "No music is playing"
 fi
