077e1008448ac14e2cf8e929471386122a182270
Cuong Vu <vuhuucuong1310@gmail.com>
2023-03-04 18:34:22 +0700

    lualine modify

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -189,6 +189,11 @@ ins_left {
 
 -- Add components to right sections
 ins_right {
+  "filetype",
+  color = { fg = colors.magenta, gui = "bold" },
+}
+
+ins_right {
   "o:encoding", -- option component same as &encoding in viml
   fmt = string.upper, -- I'm not sure why it's upper case either ;)
   cond = conditions.hide_in_width,
