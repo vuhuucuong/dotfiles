0755c76f882013eb9a6659a6facf011d46dfb0b5
Cuong Vu <vuhuucuong1310@gmail.com>
2023-08-10 01:45:40 +0700

    update sync script

--- sync.sh
+++ sync.sh
@@ -2,10 +2,12 @@
 
 
 cp -av home/. ~/ 
+echo "--------------------"
 echo "Done syncing home dotfiles!"
 
 for folder in $(ls -d -- .config/*)
 do
   rsync -avu --delete $folder ~/.config
 done
+echo "--------------------"
 echo "Done syncing config!"
