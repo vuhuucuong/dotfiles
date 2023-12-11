764002f6500b7c063d48ed0a333f6ed4f3efd1ac
Cuong Vu <vuhuucuong1310@gmail.com>
2023-12-12 01:07:18 +0700

    update starship & add diff-so-fancy

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # dotfiles
-🌟My Neovim config and relevant (.)files
-
-## Dependencies:
-### For neovim
-- CMake, make, and GCC
-- python3 > 3.8.2, python3-venv
+🌟My dotfiles
--- install.sh
+++ install.sh
@@ -7,31 +7,49 @@ echo "All home dotfiles have been copied!";
 # copy all .config
 cp -rfv .config/. "$HOME/.config/" && \
 echo ".config has been copied!";
+
 # Install fzf
 FZF_DIR="$HOME/.fzf"
 OMZ_DIR="$HOME/.oh-my-zsh"
-if [ ! -d $FZF_DIR ]; then
+if ! -d $FZF_DIR
+then
   git clone --depth 1 https://github.com/junegunn/fzf.git $FZF_DIR && \
   $FZF_DIR/install && \
   echo "fzf has been installled!";
 fi
 
 # Install oh my zsh
-if [ ! -d $OMZ_DIR ]; then
+if ! -d $OMZ_DIR
+then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
   echo "oh my zsh has been installled!";
 fi
 
 # Install zsh plugins
-if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+echo "Install zsh plugins...";
+if ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
   echo "zsh-autosuggestions has been installled!";
 fi
-if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+
+if ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
   echo "zsh-syntax-highlighting has been installled!";
 fi
-if [ ! -d ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions ]; then
+
+if ! -d ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+then
   git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions && \
   echo "zsh-completions has been installled!";
+fi
+ 
+# Install git plugins
+
+echo "Install git plugins...";
+if ! diff-so-fancy -v &> /dev/null
+then
+  sudo apt install diff-so-fancy && \
+  echo "diff-so-fancy has been installled!";
 fi
