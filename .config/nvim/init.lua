2179745c96e1b3a1035ffc04375758363f472bae
Cuong Vu <vuhuucuong1310@gmail.com>
2024-06-26 23:49:13 +0700

    update plugin nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,25 @@
-if vim.g.vscode then
-  vim.opt.clipboard = 'unnamedplus'
-else
-  -- ordinary Neovim
+-- if vim.g.vscode then
+--   vim.opt.clipboard = 'unnamedplus'
+-- else
+--   -- ordinary Neovim
+-- end
+
+vim.opt.clipboard = 'unnamedplus'
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	{ import = "vscode-neovim.plugins",    cond = (function() return vim.g.vscode end) },
+})
---
+++ .config/nvim/lua/vscode-neovim/plugins.lua
@@ -0,0 +1,15 @@
+return {{
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+}, {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}}
